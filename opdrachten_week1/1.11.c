f44d922354ea098e1ba81d1c53b0e36cc6355ad2
Jari-Nijenhuis <143879241+Jari-Nijenhuis@users.noreply.github.com>
2024-09-02 21:34:32 +0200

    Update 1.11.c

---
+++ opdrachten_week1/1.11.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+void main()
+{
+	int a, b, gcd, rest;
+
+	printf("Geef 2 getallen: ");
+	scanf("%d %d", &a, &b);
+
+	if (a < b)
+	{
+		gcd = b / a;
+
+		rest = b % gcd;
+
+	}
+	else
+	{
+		gcd = a / b;
+
+		rest = a % gcd;
+
+	}
+
+
+	printf("Grooste gene deler is %d en rest is %d", gcd, rest);
+
+}*/
