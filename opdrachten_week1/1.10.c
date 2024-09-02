97a24138fd722f6980b6a2eb2ff6e778c5447bc0
Jari-Nijenhuis <143879241+Jari-Nijenhuis@users.noreply.github.com>
2024-09-02 20:50:02 +0200

    Create 1.10.c

---
+++ opdrachten_week1/1.10.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+void main()
+{
+	float spanning,amperen;
+	int ohm;
+
+	printf("Geef spanning: ");
+	scanf("%f", &spanning);
+
+	printf("\nGeef weerstand: ");
+	scanf("%d", &ohm);
+
+	amperen = spanning / ohm;
+
+	printf(" de amperen is %.2f", amperen);
+
+
+}*/
