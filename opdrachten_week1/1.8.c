b695989f48d2f9a5612f5e11ca31120648a9d62a
Jari-Nijenhuis <143879241+Jari-Nijenhuis@users.noreply.github.com>
2024-09-02 20:49:46 +0200

    Create 1.8.c

---
+++ opdrachten_week1/1.8.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+void main()
+{
+	float PrijsEX, PrijsIN;
+
+	printf("Geef prijs excl. BTW: ");
+	scanf("%f", &PrijsEX);
+
+	PrijsIN = PrijsEX * 1.21;
+
+	printf("Prijs incl. btw %.2f", PrijsIN);
+
+}*/
