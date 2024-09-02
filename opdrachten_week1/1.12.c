4a69c894a69b86208521ddf6b786290e67a24fa9
Jari-Nijenhuis <143879241+Jari-Nijenhuis@users.noreply.github.com>
2024-09-02 21:34:29 +0200

    Create 1.12.c

---
+++ opdrachten_week1/1.12.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+void main()
+{
+	int InpuntSec ,uur, min, sec;
+
+	printf("Geef aantal seconden: ");
+	scanf("%d", &InpuntSec);
+
+	uur = InpuntSec / 3600;
+	min = (InpuntSec % 3600)/60;
+	sec = (InpuntSec % 3600 % 60);
+
+	printf("Tijd in uren %d minuten %d seconden %d", uur, min, sec);
+
+}*/
