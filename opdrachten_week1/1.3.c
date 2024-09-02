b2b528547cead70678ebfd9ad1dcfa13f8ef949c
Jari-Nijenhuis <143879241+Jari-Nijenhuis@users.noreply.github.com>
2024-09-02 18:07:51 +0200

    Create 1.3.c

---
+++ opdrachten_week1/1.3.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+void main()
+{
+	int a, b;
+
+	printf("Geeft 2 cijfers:\n");
+	scanf("%d %d", &a ,&b);
+
+	if (a == b)
+	{
+		printf("De getallen zijn het zelfde.");
+	}
+	else if(a < b)
+	{
+	  	printf("grooste getal is %d", b);
+	}
+	else
+	{
+		printf("grooste getal is %d", a);
+	}
+
+}*/
