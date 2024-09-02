9f738b5bad703e4dd39c02bf1b87b7b8c9371587
Jari-Nijenhuis <143879241+Jari-Nijenhuis@users.noreply.github.com>
2024-09-02 18:27:45 +0200

    Update 1.4.c

--- opdrachten_week1/1.4.c
+++ opdrachten_week1/1.4.c
@@ -1,7 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
+/*
 void main()
 {
+	int a, b, c;
 
-}
+	printf("Geeft 3 cijfers:\n");
+	scanf("%d %d %d", &a, &b, &c);
+
+	if (a > b && a > c)
+	{
+		printf("grooste getal is %d", a);
+	}
+	else if (b > a && b > c)
+	{
+		printf("grooste getal is %d", b);
+	}
+	else
+	{
+		printf("grooste getal is %d", c);
+	}
+
+}*/
