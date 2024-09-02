df961d6f8e2177fb146d875ea85d22c644adda72
Jari-Nijenhuis <143879241+Jari-Nijenhuis@users.noreply.github.com>
2024-09-02 21:34:26 +0200

    Create 1.13.c

---
+++ opdrachten_week1/1.13.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+void main()
+{
+	int INPUT, OUTPUT;
+
+	printf("Geef een getal: ");
+	scanf("%d", &INPUT);
+
+	OUTPUT = INPUT * INPUT;
+
+	printf("Uitkost %d", OUTPUT);
+}*/
