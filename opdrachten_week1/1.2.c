cde0c4bd7c2cfef98266b488d11df01cdb9809c5
Jari-Nijenhuis <143879241+Jari-Nijenhuis@users.noreply.github.com>
2024-09-02 17:57:56 +0200

    Update 1.2.c

--- opdrachten_week1/1.2.c
+++ opdrachten_week1/1.2.c
@@ -1,6 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 void main()
 {
+	/*int a, b, c;
 
+	printf("Geeft drie waarden:\n");
+	scanf("%d %d %d", &a, &b, &c);
+	
+	a = (a +b +c) / 3;
+	
+	printf("Het gemiddelde is %d", a);
+	*/
 }
