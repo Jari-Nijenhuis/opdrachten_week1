e89c3158446f4ef3259eba5e30d4a2c823ccdcb8
Jari-Nijenhuis <143879241+Jari-Nijenhuis@users.noreply.github.com>
2024-09-02 20:49:49 +0200

    Create 1.7.c

---
+++ opdrachten_week1/1.7.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/*
+void main()
+{
+	int radius;
+	double pi = 3.141593;
+	double oppervlakte, omtrek;
+
+	printf("Geeft een radius:  ");
+	scanf("%d", &radius);
+
+	oppervlakte = radius * radius * pi;
+
+	printf("De opervlakte is %f", oppervlakte);
+
+	omtrek = radius * pi;
+
+	printf("De omtrek is %f", omtrek);
+
+}*/
