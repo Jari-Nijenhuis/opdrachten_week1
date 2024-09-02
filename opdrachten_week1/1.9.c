c126785b57c23dfc160640972f0d6660ff2f510a
Jari-Nijenhuis <143879241+Jari-Nijenhuis@users.noreply.github.com>
2024-09-02 20:49:43 +0200

    Create 1.9.c

---
+++ opdrachten_week1/1.9.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/*
+void main()
+{
+	float MS, KMH;
+	
+
+	printf("Geef snelheid in km/h: ");
+	scanf("%f", &KMH);
+
+	MS = KMH / 3.6;
+
+	printf("Snelheid in m/s %f", MS);
+
+}*/
