e24667443b0c2acfd1319da42af6fea15cd3e03b
Jari-Nijenhuis <143879241+Jari-Nijenhuis@users.noreply.github.com>
2024-09-02 20:49:57 +0200

    Create 1.5.c

---
+++ opdrachten_week1/1.5.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+void main()
+{
+	int a, b;
+
+	printf(" geeft 2 cijfers:\n");
+	scanf("%d %d", &a, &b);
+
+	if (a == b)
+	{
+		printf("Gelijk");
+	}
+	else
+	{
+		printf("Ongelijk");
+	}
+}*/
