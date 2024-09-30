992d67f817ef6850beecb6fe31b4f8919ca599b1
Jari-Nijenhuis <143879241+Jari-Nijenhuis@users.noreply.github.com>
2024-09-30 15:12:26 +0200

    Create voorbeeld_week3.c

---
+++ opdrachten_week1/voorbeeld_week3.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+/*
+void main()
+{
+#pragma region intro
+	int a = 1;
+	int b = 2;
+
+
+	//b=3 toekening
+	//b == 3 is vergelijken
+	// && is EN
+	// ||
+
+
+	if (1 == a || 2 == b)
+	{
+		printf("huu?\n");
+	}
+
+	// NOT operator
+	int e = 13;
+	printf("Uitkomst %d\n", !e);
+
+	//kijk uit
+	// & operator
+	// | operator
+	int c = 10;
+	int d = c & 2;
+
+	printf("Uitkomst %d\n", d);
+#pragma endregion
+
+	int teller = 0;
+	while (teller <= 100)
+	{
+		printf("Teller (%d) \n", teller);
+		teller++;
+	}
+
+
+	int hans = 0;
+	while (1)
+	{
+
+		if (hans > 3)
+		{
+			break;
+		}
+
+	}
+
+}*/
